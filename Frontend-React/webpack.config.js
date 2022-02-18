0e72ca4e784651b731e12c5ff425ac9343ec0934
Neerav Panchal <masterpanchieneerav@gmail.com>
2022-02-18 10:53:31 +0530

    [18-02-22] - More stlying fixes for variables.

--- Frontend-React/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  resolve: {
-    fallback: [
-      { stream: require.resolve("stream-browserify") },
-      { zlib: require.resolve("browserify-zlib") },
-      { util: require.resolve("util/") },
-    ],
-  },
-};
